992e099d9808c18734646d6b20b6c5b7fa2aba49
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2022-12-08 09:16:59 +0300

    Reading Data

---
+++ Reading Data from the Blockchain/solana-intro-frontend/pages/index.tsx
@@ -0,0 +1,49 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import AddressForm from "../components/AddressForm";
+import * as web3 from "@solana/web3.js";
+
+const Home: NextPage = () => {
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState("");
+  const [isExecutable, setIsExecutable] = useState(false);
+
+  const addressSubmittedHandler = (address: string) => {
+    try {
+      const key = new web3.PublicKey(address);
+      setAddress(key.toBase58());
+
+      const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+
+      connection.getBalance(key).then((balance) => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL);
+      });
+
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
+      });
+
+    } catch (error) {
+      setAddress("");
+      setBalance(0);
+      alert(error);
+    }
+  };
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p>Start Your Solana Journey</p>
+        <AddressForm handler={addressSubmittedHandler} />
+        <p>{`Address: ${address}`}</p>
+        <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yes' : 'No'}`}</p>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
